90d2c797171a0a8f8ff7b7897909db69573c13e7
0xNaN <enzodicicco@gmail.com>
2015-11-06 16:51:26 +0100

    my_date_string() ex

--- chap4/time.erl
+++ chap4/time.erl
@@ -1,6 +1,6 @@
 -module(time).
 -import(timer, [now_diff/1]).
--export([my_time_func/1, loop/1]).
+-export([my_time_func/1, loop/1, my_date_string/0]).
 
 loop(0) -> done;
 loop(X) -> loop(X - 1).
@@ -13,3 +13,9 @@ my_time_func({F, Args}) ->
     F(Args),
     After = erlang:timestamp(),
     {micros, timer:now_diff(After, Before)}.
+
+my_date_string() ->
+    {Y, M, D} = date(),
+    io:format("Today is ~w/~w/~w~n", [D, M, Y]),
+    {HH, MM, SS} = time(),
+    io:format("the current time is ~w:~w:~w~n", [HH, MM, SS]).
